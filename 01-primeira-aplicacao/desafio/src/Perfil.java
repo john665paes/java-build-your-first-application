12f82fdfba2479b71de290f0547f10f96accc657
Johnatan Paes <johnatanpaes@gmail.com>
2025-03-24 20:43:33 -0300

    iniciando o curso de primeira aplicação da Alura

---
+++ 01-primeira-aplicacao/desafio/src/Perfil.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    String nome = "Johnatan";
+
+    public void main(String[] args) {
+        System.out.println("Olá " + this.nome);
+        System.out.println("Tudo bem?");
+    }
+}
